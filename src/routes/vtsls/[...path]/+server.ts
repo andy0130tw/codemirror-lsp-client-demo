397cfaebfe7f86005d4a0938549da7ff03aa2ce1
Andy Pan <andy0130tw@yahoo.com.tw>
2025-07-17 00:27:01 +0800

    Fix static resource and proxy

--- src/routes/vtsls/[...path]/+server.ts
+++ src/routes/vtsls/[...path]/+server.ts
@@ -1,11 +1,15 @@
 import { type RequestHandler } from '@sveltejs/kit'
 
+// meant to add COI headers because we do not have control on headers of static files
 export const GET = (async ({ fetch, url, params }) => {
   const response = await fetch(new URL(`/__vtsls/${params.path}`, url.origin))
   if (response.status != 200) {
     return new Response('', { headers: response.headers, status: response.status })
   }
-  return new Response(response.body, {
-    headers: response.headers,
-  });
+
+  // the server might return compressed body, but we receive it uncompressed
+  // https://answers.netlify.com/t/how-to-pass-through-the-compressed-body-of-a-fetch-response-in-edge-function/128112
+  const headers = Array.from(response.headers.entries())
+    .filter(([name]) => !name.match(/content-(encoding|length)/i))
+  return new Response(response.body, { headers });
 }) satisfies RequestHandler
